712551d15591b84dd07b6e5538951c125871d0fa
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2021-07-25 17:07:10 +0900

    fmt

--- config/config.go
+++ config/config.go
@@ -7,24 +7,23 @@ import (
 )
 
 type ConfigList struct {
-	ApiKey string
+	ApiKey    string
 	ApiSecret string
-	LogFile string
+	LogFile   string
 }
 
 var Config ConfigList
 
-func  init()  {
+func init() {
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
-		log.Printf("Failed to read file %v", err)
+		log.Printf("Failed to read file: %v", err)
 		os.Exit(1)
 	}
 
 	Config = ConfigList{
-		ApiKey: cfg.Section("bitflyer").Key("api_key").String(),
+		ApiKey:    cfg.Section("bitflyer").Key("api_key").String(),
 		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
-		LogFile: cfg.Section("gotrading").Key("log_file").String(),
+		LogFile:   cfg.Section("gotrading").Key("log_file").String(),
 	}
-
 }
